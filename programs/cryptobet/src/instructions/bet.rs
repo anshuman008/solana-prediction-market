b928bf3010b80328eee6044512480df08f32b4d8
anshuman008 <code.anshu@gmail.com>
2025-07-25 23:55:18 +0530

    test: bet isntruction and fees Cpi added

--- programs/cryptobet/src/instructions/bet.rs
+++ programs/cryptobet/src/instructions/bet.rs
@@ -1,20 +1,15 @@
 use anchor_lang::prelude::*;
 use anchor_lang::system_program::{transfer, Transfer};
 
-use crate::state::BetState;
 use crate::error::BetError;
-
-
-
-
+use crate::state::BetState;
 
 #[derive(Accounts)]
-pub struct BetStruct <'info> {
-
+pub struct BetStruct<'info> {
     #[account(mut)]
-    pub signer:Signer<'info>,
+    pub signer: Signer<'info>,
 
-    pub creator:SystemAccount<'info>,
+    pub creator: SystemAccount<'info>,
 
     #[account(
         mut,
@@ -22,52 +17,71 @@ pub struct BetStruct <'info> {
         bump = bet_state.state_bump,
         has_one = creator @ BetError::InvaildCreator,
      )]
-    pub bet_state:Account<'info,BetState>,
+    pub bet_state: Account<'info, BetState>,
 
-   #[account(
+    #[account(
     mut,
     seeds = [b"pool_account", creator.key().as_ref(),bet_state.seed.to_le_bytes().as_ref()],
     bump = bet_state.pool_bimp
    )]
-   pub pool_account:SystemAccount<'info>,
+    pub pool_account: SystemAccount<'info>,
 
-   // programs
-   pub sytem_account:Program<'info,System>
-
+    // programs
+    pub sytem_account: Program<'info, System>,
 }
 
+impl<'info> BetStruct<'info> {
+    pub fn bet(&mut self, bet: u8) -> Result<()> {
+        require!(
+            self.signer.lamports() > self.bet_state.bet_price,
+            BetError::InvalidAmount
+        );
 
-impl <'info> BetStruct <'info>{
-    
-    pub fn bet(&mut self, bet:u8) -> Result<()>{
-      
-       require!(self.signer.lamports() > self.bet_state.bet_price, BetError::InvalidAmount);
+        transfer(
+            CpiContext::new(
+                self.sytem_account.to_account_info(),
+                Transfer {
+                    from: self.signer.to_account_info(),
+                    to: self.pool_account.to_account_info(),
+                },
+            ),
+            self.bet_state.bet_price,
+        )?;
 
-        transfer(CpiContext::new(self.sytem_account.to_account_info(),
-         Transfer { 
-            from: self.signer.to_account_info(), 
-            to: self.pool_account.to_account_info() 
+        if bet == 0 {
+            self.bet_state.no_voters.push(self.signer.key());
+        } else if bet == 1 {
+            self.bet_state.yes_voters.push(self.signer.key());
         }
-    ), self.bet_state.bet_price)?;
 
+        self.bet_state
+            .total_transactions
+            .checked_add(1)
+            .ok_or(BetError::ArithmeticError)?;
 
-       if bet == 0 {
-        self.bet_state.no_voters.push(self.signer.key());
-       }else if bet == 1{
-        self.bet_state.yes_voters.push(self.signer.key());
-       }
+        let game_fees: u64 = self
+            .bet_state
+            .betfees
+            .try_into()
+            .map_err(|_| BetError::ArithmeticError)?;
 
-       self.bet_state.total_transactions.checked_add(1).ok_or(BetError::ArithmeticError);
+        let fees = game_fees
+            .checked_mul(self.bet_state.bet_price)
+            .ok_or(BetError::ArithmeticError)?
+            .checked_div(10000)
+            .ok_or(BetError::ArithmeticError)?;
 
+        transfer(
+            CpiContext::new(
+                self.sytem_account.to_account_info(),
+                Transfer {
+                    from: self.signer.to_account_info(),
+                    to: self.creator.to_account_info(),
+                },
+            ),
+            fees,
+        )?;
 
-       Ok(())
-
-
+        Ok(())
     }
-
-
-
- 
-    
-
 }
