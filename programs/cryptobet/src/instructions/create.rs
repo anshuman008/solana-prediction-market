71e8b5a2c43550636eebae2a3b47051bf81fd402
anshuman008 <code.anshu@gmail.com>
2025-07-26 23:59:10 +0530

    feat: writing tests

--- programs/cryptobet/src/instructions/create.rs
+++ programs/cryptobet/src/instructions/create.rs
@@ -1,93 +1,111 @@
-use anchor_lang::prelude::*;
-use crate::state::BetState;
-use pyth_solana_receiver_sdk::price_update::{get_feed_id_from_hex, PriceUpdateV2};
 use crate::error::BetError;
-
+use crate::state::BetState;
+use anchor_lang::prelude::*;
+use pyth_solana_receiver_sdk::price_update::{get_feed_id_from_hex, PriceUpdateV2};
 
 #[derive(Accounts)]
 #[instruction(seed:u16)]
-pub struct CreateStruct <'info> {
-
+pub struct CreateStruct<'info> {
     #[account(mut)]
-    pub signer:Signer<'info>,
+    pub signer: Signer<'info>,
 
-     #[account(
+    #[account(
         init,
         payer = signer,
         space = BetState::INIT_SPACE + BetState::DISCRIMINATOR.len(),
         seeds = [b"bet_state", signer.key().as_ref(),seed.to_le_bytes().as_ref()],
         bump
      )]
-     pub bet_state:Account<'info,BetState>,
+    pub bet_state: Account<'info, BetState>,
 
-     #[account(
+    #[account(
         mut,
         seeds = [b"pool_account", signer.key().as_ref(),seed.to_le_bytes().as_ref()],
         bump
      )]
-     pub pool_account:SystemAccount<'info>,
+    pub pool_account: SystemAccount<'info>,
 
-     pub pyth_price_feed: Account<'info, PriceUpdateV2>,
+    pub pyth_price_feed: Account<'info, PriceUpdateV2>,
 
-     pub system_program:Program<'info,System>,
-
-
+    pub system_program: Program<'info, System>,
 }
 
+impl<'info> CreateStruct<'info> {
+    pub fn initialize(
+        &mut self,
+        seed: u16,
+        crypto_target_price: u64,
+        bet_price: u64,
+        bet_duration: u64,
+        betfees: u16,
+        state_bump: u8,
+        pool_bump: u8,
+    ) -> Result<()> {
+        let clock = Clock::get()?;
+        let current_time = clock.unix_timestamp as u64;
 
-impl <'info> CreateStruct <'info> {
-    
+        require!(bet_duration >= 120, BetError::InvalidTime);
+        require!(crypto_target_price > 0, BetError::InvalidTarget);
+        require!(bet_price > 0, BetError::InvalidBet);
 
-    pub fn initialize(&mut self,seed:u16,crypto_target_price:u64, bet_price:u64,bet_duration:u64,betfees:u16 ,state_bump:u8,pool_bump:u8 ) -> Result<()>{
+        let price_update = &mut self.pyth_price_feed;
+        let maximum_age: u64 = 30;
 
-         let clock = Clock::get()?;
-         let current_time = clock.unix_timestamp as u64;
+        // btc/usd
+        let feed_id: [u8; 32] = get_feed_id_from_hex(
+            "0xe62df6c8b4a85fe1a67db44dc12de5db330f7ac66b72dc658afedf0f4a415b43",
+        )?;
+        let price: pyth_solana_receiver_sdk::price_update::Price =
+            price_update.get_price_no_older_than(&clock, maximum_age, &feed_id)?;
 
-         require!(bet_duration >= 120,BetError::InvalidTime);
-         require!(crypto_target_price > 0, BetError::InvalidTarget);
-         require!(bet_price > 0, BetError::InvalidBet);
+        msg!(
+            "The price is ({} ± {}) * 10^{}",
+            price.price,
+            price.conf,
+            price.exponent
+        );
 
-
-          let price_update = &mut self.pyth_price_feed;
-          let maximum_age: u64 = 30;
-
-          // btc/usd
-         let feed_id: [u8; 32] = get_feed_id_from_hex("0xe62df6c8b4a85fe1a67db44dc12de5db330f7ac66b72dc658afedf0f4a415b43")?;
-         let price: pyth_solana_receiver_sdk::price_update::Price = price_update.get_price_no_older_than(&clock, maximum_age, &feed_id)?;
-
-
-         msg!("The price is ({} ± {}) * 10^{}", price.price, price.conf, price.exponent);
-
-         let crypto_start_price = (price.price as f64 * 10f64.powi(price.exponent)) as u64;
+        let crypto_start_price = (price.price as f64 * 10f64.powi(price.exponent)) as u64;
 
         self.bet_state.set_inner(BetState {
-             creator: self.signer.key(),
-             bet_price: bet_price,
-             crypto_start_price: crypto_start_price,
-             crypto_traget_price: crypto_target_price,
-             yes_voters: [].to_vec(),
-             is_active: true,
-             winner_side: -1,
-             no_voters: [].to_vec(), 
-             start_duration: current_time,
-             bet_duration: bet_duration, 
-             total_transactions: 0, 
-             state_bump: state_bump, 
-             pool_bump: pool_bump, 
-             betfees: betfees,
-             seed: seed 
-            });
+            creator: self.signer.key(),
+            bet_price: bet_price,
+            crypto_start_price: crypto_start_price,
+            crypto_traget_price: crypto_target_price,
+            yes_voters: [].to_vec(),
+            is_active: true,
+            winner_side: -1,
+            no_voters: [].to_vec(),
+            start_duration: current_time,
+            bet_duration: bet_duration,
+            total_transactions: 0,
+            state_bump: state_bump,
+            pool_bump: pool_bump,
+            betfees: betfees,
+            seed: seed,
+        });
 
-
-            Ok(())
+        Ok(())
     }
-
-
 }
 
+pub fn createhandler(
+    ctx: Context<CreateStruct>,
+    seed: u16,
+    crypto_target_price: u64,
+    bet_price: u64,
+    bet_duration: u64,
+    betfees: u16,
+) -> Result<()> {
+    ctx.accounts.initialize(
+        seed,
+        crypto_target_price,
+        bet_price,
+        bet_duration,
+        betfees,
+        ctx.bumps.bet_state,
+        ctx.bumps.pool_account,
+    )?;
 
-pub fn createhandler(ctx:Context<CreateStruct>,seed:u16,crypto_target_price:u64, bet_price:u64,bet_duration:u64,betfees:u16 ) -> Result<()>{
-     ctx.accounts.initialize(seed, crypto_target_price, bet_price, bet_duration, betfees, ctx.bumps.bet_state, ctx.bumps.pool_account,)?;
-
-     Ok(())
+    Ok(())
 }
--- tests/cryptobet.ts
+++ tests/cryptobet.ts
@@ -1,16 +1,69 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Cryptobet } from "../target/types/cryptobet";
+import { PublicKey, Keypair, SystemProgram, LAMPORTS_PER_SOL, clusterApiUrl } from "@solana/web3.js";
+import { expect } from "chai";
+import { PythSolanaReceiver } from "@pythnetwork/pyth-solana-receiver";
+import { Connection } from "@solana/web3.js";
+import NodeWallet from "@coral-xyz/anchor/dist/cjs/nodewallet";
+import { it } from "mocha";
 
-describe("cryptobet", () => {
+describe("cryptobet", async() => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.cryptobet as Program<Cryptobet>;
+  const provider = anchor.AnchorProvider.env(); 
+  const connection = provider.connection;
+  const wallet = provider.wallet as anchor.Wallet;
+  const pythSolanaReceiver = new PythSolanaReceiver({ connection, wallet });
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
-});
+
+
+  const solUsdPriceFeedAccount = pythSolanaReceiver
+  .getPriceFeedAccountAddress(0, "0xe62df6c8b4a85fe1a67db44dc12de5db330f7ac66b72dc658afedf0f4a415b43")
+  .toBase58();
+
+
+  console.log(solUsdPriceFeedAccount);
+
+
+   
+  it("it shoud intialize the markte", async() => {
+
+    const signer = provider.publicKey;
+    const seed = new anchor.BN(1234);
+
+    // state account
+    const state_seed = [Buffer.from("bet_state"),signer.toBuffer(),seed.toArrayLike(Buffer,"le",8)];
+    const state_account =  getpda(state_seed);
+
+    // bet account 
+    const bet_seed = [Buffer.from("pool_account"),signer.toBuffer(),seed.toArrayLike(Buffer,"le",8)];
+    const bet_account = getpda(bet_seed);
+
+    console.log("here is state_account", state_account.toBase58());
+    console.log("here is bet_account", bet_account.toBase58())
+
+    console.log("passed")
+  })
+
+
+
+
+
+
+
+  const getpda = (seeds:any) => {
+    return PublicKey.findProgramAddressSync(
+      seeds,
+      program.programId
+    )[0]
+  }
+
+
+
+  
+})
+
+
